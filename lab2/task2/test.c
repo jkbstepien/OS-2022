584bfecbd11cb1944f382902318b77899754aa26
jkbstepien <devjakub.stepien@gmail.com>
2022-03-20 17:20:15 +0100

    50% done - task 2

---
+++ lab2/task2/test.c
@@ -0,0 +1,134 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <sys/times.h>
+
+clock_t start_time, end_time;
+struct tms timer_start_tms, timer_end_tms;
+
+void start_timer();
+void end_timer();
+double calc_time(clock_t start, clock_t end);
+void print_time(FILE* file, int count_c, int count_lines);
+
+int libCountSign(char sign, char* file);
+int libCountLines(char sign, char* file);
+
+int main(int argc, char** argv){
+    char* character;
+    char* buffer;
+
+    // Validate number of program arguments.
+    if (argc > 3) {
+        printf("Incorrect number of arguments. Expected 2 arguments.");
+        return -1;
+    }
+    else {
+        if (argc == 1 || argc == 2) {
+            // If arguments are lacking - prompt user to fill them.
+            printf("Character to search for and source file:\n");
+
+            character = calloc(256, sizeof(char));
+            buffer = calloc(256, sizeof(char));
+
+            scanf("%c", character);
+            scanf("%s", buffer);
+        }
+        else {
+            character = argv[1][0];
+            buffer = argv[2];
+        }
+    }
+
+    start_timer();
+
+    FILE* result_file = fopen("pomiar_zad_2.txt", "w");
+
+    int count_characters = libCountSign(character, buffer);
+    int count_clines = libCountLines(character, buffer);
+
+    end_timer();
+    print_time(result_file, count_characters, count_clines);
+
+    fclose(result_file);
+
+    return 0;
+}
+
+void start_timer() {
+    start_time = times(&timer_start_tms);
+}
+
+void end_timer() {
+    end_time = times(&timer_end_tms);
+}
+
+double calc_time(clock_t start, clock_t end) {
+    return (double)(end - start)/ (double)sysconf(_SC_CLK_TCK);
+}
+
+void print_time(FILE* file, int count_c, int count_clines) {
+    fprintf(file, "Searching this file returned following results:\n\n");
+    fprintf(file, "\tThere are %d characters in total %d lines.\n",
+        count_c,
+        count_clines);
+    fprintf(file, "\tMeasured times:\n\t\treal: %.4f user: %.4f sys: %.4f\n",
+        calc_time(start_time, end_time),
+        calc_time(timer_start_tms.tms_cutime, timer_end_tms.tms_cutime),
+        calc_time(timer_start_tms.tms_cstime, timer_end_tms.tms_cstime));
+}
+
+int libCountSign(char sign, char* file){
+    FILE* input_file1 = fopen(file, "r");
+
+    if (input_file1 == NULL){
+        return -1;
+    }
+
+    char c;
+    int signCounter = 0;
+    do{
+        fread(&c, sizeof(char), 1, input_file1);
+        if (c == sign){
+            signCounter ++;
+        }
+
+    }while(feof(input_file1) == 0);
+    fclose(input_file1);
+
+    return signCounter;
+}
+
+
+int libCountLines(char sign, char* file){
+    FILE* input_file2 = fopen(file, "r");
+
+    if (input_file2 == NULL){
+        return -1;
+    }
+
+    char c;
+    int lineCounter = 0;
+    int newLineSignCounter = 0;
+    do{
+        fread(&c, sizeof(char), 1, input_file2);
+        if((int) c == '\n'){
+            if(newLineSignCounter != 0){
+                lineCounter ++;
+                newLineSignCounter = 0;
+            }
+        }
+        if (c == sign){
+            if (newLineSignCounter == 0){
+                newLineSignCounter ++;
+            }
+        }
+
+    } while(feof(input_file2) == 0);
+    fclose(input_file2);
+
+    return lineCounter;
+}
