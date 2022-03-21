4e4fb7f1cf7389656f84750105dd66227377cb53
jkbstepien <devjakub.stepien@gmail.com>
2022-03-21 11:11:10 +0100

    cleaning lab 1 tasks

--- lab1/task1/mylib.h
+++
@@ -1,9 +0,0 @@
-#ifndef LAB1_OS_2022_MYLIB_H
-#define LAB1_OS_2022_MYLIB_H
-
-void create_table(int size);
-void wc_files(char* file_desc);
-int load_block_to_table();
-void remove_block(int index);
-
-#endif //LAB1_OS_2022_MYLIB_H
